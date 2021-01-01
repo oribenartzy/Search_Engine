ed123f4fef6ca290e0373c0595a780748ab8f6e5
אורי בן-ארצי <oribena@post.bgu.ac.il>
2021-01-01 16:02:20 +0200

    1.1.21

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 class ConfigClass:
     def __init__(self):
-        self.corpusPath = ''
+        self.corpusPath = 'C:\\Users\\אורי בן-ארצי\\PycharmProjects\\Search_Engine\\data'
         self.savedFileMainFolder = ''
         self.saveFilesWithStem = self.savedFileMainFolder + "/WithStem"
         self.saveFilesWithoutStem = self.savedFileMainFolder + "/WithoutStem"
